66993c3707ceb90bad95a33f93876b26e57f4e2e
yejinlong <415571973@qq.com>
2025-02-18 21:04:52 +0800

    安装前先检查是否需要卸载

--- custominst/main.cpp
+++ custominst/main.cpp
@@ -1,7 +1,7 @@
 ﻿#include <QCoreApplication>
 #include <Windows.h>
 
-std::wstring GetSoftInstallPath()
+std::wstring getSoftInstallPath()
 {
     static std::wstring strSoftInstallPath;
     if (!strSoftInstallPath.empty())
@@ -25,61 +25,9 @@ std::wstring GetSoftInstallPath()
     return strSoftInstallPath;
 }
 
-int main(int argc, char *argv[])
+// 注册/卸载应用scheme
+void registerAppScheme(bool isInstall)
 {
-    QCoreApplication a(argc, argv);
-
-    if (argc < 2)
-    {
-        qCritical("too less params");
-        return 1;
-    }
-
-    bool isInstall = true;
-    if (strcmp(argv[1], "install") == 0)
-    {
-        isInstall = true;
-    }
-    else if (strcmp(argv[1], "uninstall") == 0)
-    {
-        isInstall = false;
-    }
-    else
-    {
-        qCritical("wrong param");
-        return 2;
-    }
-
-    // 安装或卸载虚拟摄像头
-    SHELLEXECUTEINFO sei;
-    ZeroMemory(&sei, sizeof(sei));
-    sei.cbSize = sizeof(sei);
-    sei.fMask = SEE_MASK_NOCLOSEPROCESS;
-    sei.lpVerb = L"runas";
-    std::wstring installerFilePath = GetSoftInstallPath() + L"x64\\jericcam_installer.exe";
-    sei.lpFile = installerFilePath.c_str();
-    std::wstring dllFilePath = GetSoftInstallPath() + L"x64\\jericcam.dll";
-    wchar_t parameter[2*MAX_PATH];
-    if (isInstall)
-    {
-        _snwprintf_s(parameter, 2*MAX_PATH, L"register \"%s\" 1", dllFilePath.c_str());
-    }
-    else
-    {
-        _snwprintf_s(parameter, 2*MAX_PATH, L"unregister \"%s\"", dllFilePath.c_str());
-    }
-    sei.lpParameters = parameter;
-
-    if (!ShellExecuteEx(&sei))
-    {
-        qCritical("failed to start virtual camera installer, error is %d", GetLastError());
-        return 3;
-    }
-
-    WaitForSingleObject(sei.hProcess, INFINITE);
-    CloseHandle(sei.hProcess);
-
-    // 注册应用scheme，先删除，如果是安装再创建
     HKEY hRootKey;
     LPCWSTR subKey = L"akool";
     if (RegOpenKeyEx(HKEY_CLASSES_ROOT, NULL, 0, KEY_WRITE, &hRootKey) == ERROR_SUCCESS)
@@ -92,7 +40,7 @@ int main(int argc, char *argv[])
     {
         HKEY hKey;
         LPCWSTR defaultValue = L"URL:akool";
-        std::wstring mainFilePath = GetSoftInstallPath() + L"main.exe";
+        std::wstring mainFilePath = getSoftInstallPath() + L"main.exe";
         std::wstring command = std::wstring(L"\"") + mainFilePath + L"\" --open-url=\"%1\"";
         LPCWSTR commandValue = command.c_str();
         if (RegCreateKeyEx(HKEY_CLASSES_ROOT, subKey, 0, NULL, REG_OPTION_NON_VOLATILE, KEY_WRITE, NULL, &hKey, NULL) == ERROR_SUCCESS)
@@ -148,6 +96,104 @@ int main(int argc, char *argv[])
             qCritical("Failed to create or open 'akoolcamera' key.");
         }
     }
+}
+
+// 注册/删除应用信息：安装路径
+void registerAppInfo(bool isInstall)
+{
+    HKEY hRootKey;
+    if (RegOpenKeyEx(HKEY_LOCAL_MACHINE, NULL, 0, KEY_WRITE, &hRootKey) == ERROR_SUCCESS)
+    {
+        HKEY softwareKey;
+        if (RegOpenKeyEx(hRootKey, L"SOFTWARE", 0, KEY_WRITE, &softwareKey) == ERROR_SUCCESS)
+        {
+            LPCWSTR subKey = L"AkoolCam";
+            RegDeleteTree(softwareKey, subKey);
+
+            if (isInstall)
+            {
+                HKEY hKey;
+                if (RegCreateKeyEx(softwareKey, subKey, 0, NULL, REG_OPTION_NON_VOLATILE, KEY_WRITE, NULL, &hKey, NULL) == ERROR_SUCCESS)
+                {
+                    std::wstring installPath = getSoftInstallPath();
+                    if (RegSetValueEx(hKey, L"InstallPath", 0, REG_SZ, (const BYTE*)installPath.c_str(), (installPath.length()+1)*2) != ERROR_SUCCESS)
+                    {
+                        qCritical("Failed to set install path value.");
+                    }
+                    RegCloseKey(hKey);
+                }
+                else
+                {
+                    qCritical("Failed to create akool key");
+                }
+            }
+
+            RegCloseKey(softwareKey);
+        }
+
+        RegCloseKey(hRootKey);
+    }
+}
+
+int main(int argc, char *argv[])
+{
+    QCoreApplication a(argc, argv);
+
+    if (argc < 2)
+    {
+        qCritical("too less params");
+        return 1;
+    }
+
+    bool isInstall = true;
+    if (strcmp(argv[1], "install") == 0)
+    {
+        isInstall = true;
+    }
+    else if (strcmp(argv[1], "uninstall") == 0)
+    {
+        isInstall = false;
+    }
+    else
+    {
+        qCritical("wrong param");
+        return 2;
+    }
+
+    // 安装或卸载虚拟摄像头
+    SHELLEXECUTEINFO sei;
+    ZeroMemory(&sei, sizeof(sei));
+    sei.cbSize = sizeof(sei);
+    sei.fMask = SEE_MASK_NOCLOSEPROCESS;
+    sei.lpVerb = L"runas";
+    std::wstring installerFilePath = getSoftInstallPath() + L"x64\\jericcam_installer.exe";
+    sei.lpFile = installerFilePath.c_str();
+    std::wstring dllFilePath = getSoftInstallPath() + L"x64\\jericcam.dll";
+    wchar_t parameter[2*MAX_PATH];
+    if (isInstall)
+    {
+        _snwprintf_s(parameter, 2*MAX_PATH, L"register \"%s\" 1", dllFilePath.c_str());
+    }
+    else
+    {
+        _snwprintf_s(parameter, 2*MAX_PATH, L"unregister \"%s\"", dllFilePath.c_str());
+    }
+    sei.lpParameters = parameter;
+
+    if (!ShellExecuteEx(&sei))
+    {
+        qCritical("failed to start virtual camera installer, error is %d", GetLastError());
+        return 3;
+    }
+
+    WaitForSingleObject(sei.hProcess, INFINITE);
+    CloseHandle(sei.hProcess);
+
+    // 注册应用scheme，先删除，如果是安装再创建
+    registerAppScheme(isInstall);
+
+    // 注册应用信息
+    registerAppInfo(isInstall);
 
     return 0;
 }
--- setup/config/controlscript.qs
+++ setup/config/controlscript.qs
@@ -28,59 +28,95 @@
 
 function Controller()
 {    
-    installer.setDefaultPageVisible(QInstaller.LicenseCheck, 0);
-    installer.setDefaultPageVisible(QInstaller.StartMenuSelection, 0);
-    installer.setDefaultPageVisible(QInstaller.ComponentSelection, 0);
-    
+    installer.setDefaultPageVisible(QInstaller.LicenseCheck, 0)
+    installer.setDefaultPageVisible(QInstaller.StartMenuSelection, 0)
+    installer.setDefaultPageVisible(QInstaller.ComponentSelection, 0)    
     if (installer.isInstaller())
     {
-        installer.setDefaultPageVisible(QInstaller.ReadyForInstallation, 0);        
+        installer.setDefaultPageVisible(QInstaller.ReadyForInstallation, 0)        
     }
-    installer.currentPageChanged.connect(onCurrentPageChanged);
+	
+    installer.currentPageChanged.connect(onCurrentPageChanged)
+}
+
+uninstallIfHasInstalled = function() {
+	if (!installer.isInstaller()) {
+		return
+	}
+	
+	// 如果已安装，就先卸载
+	var registryPath = "HKEY_LOCAL_MACHINE\\SOFTWARE\\AkoolCam\\InstallPath"
+	var installPath = installer.value(registryPath, "", QSettings.Registry32Format)
+	if (installPath.length == 0) {
+		console.log("not install")
+		return
+	}
+	
+	console.log("install path: " + installPath)
+	var uninstallerPath = installPath + "uninstall.exe"
+	if (!installer.fileExists(uninstallerPath)) {
+		console.log("uninstall program not exist")
+		return
+	}
+	
+	QMessageBox.information("uninstallConfirm", "Tip", "Uninstall the previous version now", QMessageBox.Yes)
+	
+	installer.execute("\"" + uninstallerPath + "\"")
+	
+	installPath = installer.value(registryPath, "", QSettings.Registry32Format)
+	if (installPath.length == 0) {
+		console.log("uninstall successfully")
+	}
+	else {
+		console.log("uninstall is cancelled")
+		installer.interrupt()
+	}
 }
 
 onCurrentPageChanged = function(page)
 {
-    if (page == QInstaller.TargetDirectory)
+	if (page == QInstaller.TargetDirectory)
     {
-        components = installer.components("");
+		uninstallIfHasInstalled()
+	
+        components = installer.components("")
         for (var i=0; i<components.length; i++)
         {
-            installer.selectComponent(components[i].name);
+            installer.selectComponent(components[i].name)
         }
     }
     else if (page == QInstaller.InstallationFinished)
     {
         if (installer.isInstaller())
         {     
-            customInstall("install");
+            customInstall("install")
         }
     }
     else if (page == QInstaller.PerformInstallation)
     {        
         if (installer.isUninstaller())
         {            
-            customInstall("uninstall");
+            customInstall("uninstall")
         }
     }
 }
 
 customInstall = function(installParam)
 {
-    var installDir = installer.value("TargetDir");
-    var shellPath = "\"" + installDir + "\\shellexec.exe" + "\"";
-    var customInstallerPath = "\"" + installDir + "\\custominst.exe" + "\"";
+    var installDir = installer.value("TargetDir")
+    var shellPath = "\"" + installDir + "\\shellexec.exe" + "\""
+    var customInstallerPath = "\"" + installDir + "\\custominst.exe" + "\""
     var params = [customInstallerPath, installParam]
-    console.log("shell path: " + shellPath);
-    console.log("custom installer path: " + customInstallerPath);
-    result = installer.execute(shellPath, params);
+    console.log("shell path: " + shellPath)
+    console.log("custom installer path: " + customInstallerPath)
+    result = installer.execute(shellPath, params)
     if (result.length == 0)
     {
-        console.log("failed to run the program");
+        console.log("failed to run the program")
     }
     else
     {
-        console.log("output: " + result[0]);
-        console.log("code: " + result[1]);
+        console.log("output: " + result[0])
+        console.log("code: " + result[1])
     }
 }
